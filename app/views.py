b3c11a1d96468d2eeb680ff1516959b49c0a5a4a
VaishnaviNagalooti <nagalootivaish@gmail.com>
2024-01-23 20:51:29 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,8 +3,9 @@ from django.core.mail import send_mail
 # Create your views here.
 
 from app.forms import *
-from django.http import HttpResponse
-
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
 
 def registration(request):
     ufo=UserForm()
@@ -28,3 +29,22 @@ def registration(request):
             return HttpResponse('Invalid Data')
 
     return render(request,'registration.html',d)
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active :
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('u r not an authenticated user')
+    return render(request,'user_login.html')
